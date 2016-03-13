76f18c9d32e98683d7090377c964e9797169bb14
Rémi Becheras <rbecheras@sirap.fr>
2016-03-14 00:56:44 +0100

    update the group-factory.provider documentation annotations

--- src/group-factory.provider.js
+++ src/group-factory.provider.js
@@ -3,29 +3,63 @@
 /**
  * @ngdoc service
  * @serviceProvider
+ * @constructor
  * @name ui.selectTree.provider:groupFactory
  * @requires ui.select
  * @module ui.selectTree
  * @function
  *
  * @description
- * Provides tree selection on top of angular-ui/ui-select
+ * The factory is used by directives to load the ui-select list with children on selection change
  *
  */
-module.exports = [function(){
+ function groupFactoryProvider(){
 
-  var tree;
+   /**
+    * The tree data store
+    * @property
+    * @private
+    *
+    */
+   var tree;
 
-  this.loadTree = function(data){
-    tree = data;
-  };
+   /**
+    * The method used in a config block, to load a full tree formatted data in the factory.
+    * @method
+    * @param {object} data An indexed object (keys are numbers starting at zero) where members are arrays of objects (items to select)
+    * @returns void
+    */
+   this.loadTree = function(data){
+     tree = data;
+   };
 
-  this.$get = function(){
-    return {
-      load: function (id) {
-        return tree[id];
-      }
-    };
-  };
+   /**
+    * Implementation of the $get method of the angular module#factory interface. It returns the groupFactory service.
+    * @method
+    * @returns {object} the groupFactory instance.
+    */
+   this.$get = function(){
+     /**
+      * The groupFactory instance.
+      * @name groupFactory
+      * @type {object}
+      */
+     return {
+       /**
+        * Load the ui-select list by a sub-group given by its specific id.
+        * @method
+        * @memberof groupFactory
+        * @param {integer} id The item id.
+        * @returns The requested item.
+        */
+       load: function (id) {
+         return tree[id];
+       }
+     };
+   };
 
-}];
+ }
+
+// Annotate the angular factory, and export it.
+// No dependency, so no supplementary annotation, just wrap the factory in an array.
+module.exports = [groupFactoryProvider];
